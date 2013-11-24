a2082742b3c72716e28cf6e0274b41e54889e59b
Heiher <r@hev.cc>
2013-11-24 12:02:14 +0800

    Add common header 'hev-lib.h'.

---
+++ include/hev-lib.h
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : hev-lib.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2013 everyone.
+ Description : Public header
+ ============================================================================
+ */
+
+#ifndef __HEV_LIB_H__
+#define __HEV_LIB_H__
+
+#include <hev-async-queue.h>
+#include <hev-event-loop.h>
+#include <hev-event-source.h>
+#include <hev-list.h>
+#include <hev-memory-allocator.h>
+#include <hev-queue.h>
+#include <hev-slist.h>
+
+#endif /* __HEV_LIB_H__ */
+
