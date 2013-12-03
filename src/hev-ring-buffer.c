6b364e6460c875aae4a18a8f4e182217fb739942
Heiher <r@hev.cc>
2013-12-03 22:54:57 +0800

    HevRingBuffer: Refactor.

--- src/hev-ring-buffer.c
+++ src/hev-ring-buffer.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdint.h>
+#include <assert.h>
 
 #include "hev-ring-buffer.h"
 #include "hev-memory-allocator.h"
@@ -29,6 +30,7 @@ hev_ring_buffer_new (size_t len)
 	HevRingBuffer *self = NULL;
 	self = HEV_MEMORY_ALLOCATOR_ALLOC (sizeof (HevRingBuffer) + len);
 	if (self) {
+		self->ref_count = 1;
 		self->wp = 0;
 		self->rp = 0;
 		self->len = len;
@@ -62,18 +64,103 @@ size_t
 hev_ring_buffer_reading (HevRingBuffer *self, struct iovec *iovec)
 {
 	if (self && iovec) {
-		ssize_t len = self->wp - self->rp;
+		if (0 == self->rp) {
+			/* rp
+			 * ||--------------------||
+			 * wp */
+			if ((self->rp == self->wp) && self->full) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->len;
+				return 1;
+			}
+			/* rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->len > self->wp)) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->wp;
+				return 1;
+			}
+			/* rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->wp;
+				return 1;
+			}
+		}
+		if ((0 < self->rp) && (self->len > self->rp)) {
+			/*           rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				iovec[0].iov_base = self->buffer + self->rp;
+				iovec[0].iov_len = self->len - self->rp;
+				return 1;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *      wp */
+			if ((0 < self->wp) && (self->wp < self->rp)) {
+				iovec[0].iov_base = self->buffer + self->rp;
+				iovec[0].iov_len = self->len - self->rp;
+				iovec[1].iov_base = self->buffer;
+				iovec[1].iov_len = self->wp;
+				return 2;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *           wp */
+			if ((self->wp == self->rp) && self->full) {
+				iovec[0].iov_base = self->buffer + self->rp;
+				iovec[0].iov_len = self->len - self->rp;
+				iovec[1].iov_base = self->buffer;
+				iovec[1].iov_len = self->wp;
+				return 2;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                wp */
+			if ((self->wp > self->rp) && (self->wp < self->len)) {
+				iovec[0].iov_base = self->buffer + self->rp;
+				iovec[0].iov_len = self->wp - self->rp;
+				return 1;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				iovec[0].iov_base = self->buffer + self->rp;
+				iovec[0].iov_len = self->wp - self->rp;
+				return 1;
+			}
+		}
+		if (self->len == self->rp) {
+			/*                       rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				return 0;
+			}
 
-		if ((0 > len) || ((0 == len) && self->full)) {
-			iovec[0].iov_base = self->buffer + self->rp;
-			iovec[0].iov_len = self->len - self->rp;
-			iovec[1].iov_base = self->buffer;
-			iovec[1].iov_len = self->wp;
-			return 2;
-		} else if (0 < len) {
-			iovec[0].iov_base = self->buffer + self->rp;
-			iovec[0].iov_len = len;
-			return 1;
+			/*                       rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->wp < self->len)) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->wp;
+				return 1;
+			}
+
+			/*                       rp
+			 * ||--------------------||
+			 *                       wp */
+			if ((self->wp == self->rp) && self->full) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->wp;
+				return 1;
+			}
 		}
 	}
 
@@ -83,35 +170,212 @@ hev_ring_buffer_reading (HevRingBuffer *self, struct iovec *iovec)
 void
 hev_ring_buffer_read_finish (HevRingBuffer *self, size_t inc_len)
 {
-	if (self) {
-		if (0 < inc_len) {
-			size_t p = inc_len + self->rp;
-			if (self->len < p)
-			  self->rp = p - self->len;
-			else
-			  self->rp = p;
+	if (self && (0 < inc_len)) {
+		if (0 == self->rp) {
+			/* rp
+			 * ||--------------------||
+			 * wp */
+			if ((self->rp == self->wp) && self->full) {
+				assert (self->len >= inc_len);
+				self->rp = inc_len;
+				self->full = false;
+				return;
+			}
+			/* rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->len > self->wp)) {
+				assert (self->wp >= inc_len);
+				self->rp = inc_len;
+				return;
+			}
+			/* rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				assert (self->len >= inc_len);
+				self->rp = inc_len;
+				return;
+			}
 		}
-		if (self->wp == self->rp)
-		  self->full = false;
+		if ((0 < self->rp) && (self->len > self->rp)) {
+			/*           rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				assert ((self->len - self->rp) >= inc_len);
+				self->rp += inc_len;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *      wp */
+			if ((0 < self->wp) && (self->wp < self->rp)) {
+				assert ((self->len - self->rp + self->wp) >= inc_len);
+				self->rp += inc_len;
+				if (self->rp > self->len)
+				  self->rp -= self->len;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *           wp */
+			if ((self->wp == self->rp) && self->full) {
+				assert ((self->len - self->rp + self->wp) >= inc_len);
+				self->rp += inc_len;
+				if (self->rp > self->len)
+				  self->rp -= self->len;
+				self->full = false;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                wp */
+			if ((self->wp > self->rp) && (self->wp < self->len)) {
+				assert ((self->wp - self->rp) >= inc_len);
+				self->rp += inc_len;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				assert ((self->wp - self->rp) >= inc_len);
+				self->rp += inc_len;
+				return;
+			}
+		}
+		if (self->len == self->rp) {
+			/*                       rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				return;
+			}
+
+			/*                       rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->wp < self->len)) {
+				assert (self->wp >= inc_len);
+				self->rp = inc_len;
+				return;
+			}
+
+			/*                       rp
+			 * ||--------------------||
+			 *                       wp */
+			if ((self->wp == self->rp) && self->full) {
+				assert (self->wp >= inc_len);
+				self->rp = inc_len;
+				self->full = false;
+				return;
+			}
+		}
 	}
 }
 
 size_t
 hev_ring_buffer_writing (HevRingBuffer *self, struct iovec *iovec)
 {
-	if (self) {
-		ssize_t len = self->rp - self->wp;
+	if (self && iovec) {
+		if (0 == self->rp) {
+			/* rp
+			 * ||--------------------||
+			 * wp */
+			if ((self->rp == self->wp) && !self->full) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->len;
+				return 1;
+			}
+			/* rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->len > self->wp)) {
+				iovec[0].iov_base = self->buffer + self->wp;
+				iovec[0].iov_len = self->len - self->wp;
+				return 1;
+			}
+			/* rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				return 0;
+			}
+		}
+		if ((0 < self->rp) && (self->len > self->rp)) {
+			/*           rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->rp - self->wp;
+				return 1;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *      wp */
+			if ((0 < self->wp) && (self->wp < self->rp)) {
+				iovec[0].iov_base = self->buffer + self->wp;
+				iovec[0].iov_len = self->rp - self->wp;
+				return 1;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *           wp */
+			if ((self->wp == self->rp) && !self->full) {
+				iovec[0].iov_base = self->buffer + self->wp;
+				iovec[0].iov_len = self->len - self->wp;
+				iovec[1].iov_base = self->buffer;
+				iovec[1].iov_len = self->rp;
+				return 2;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                wp */
+			if ((self->wp > self->rp) && (self->wp < self->len)) {
+				iovec[0].iov_base = self->buffer + self->wp;
+				iovec[0].iov_len = self->len - self->wp;
+				iovec[1].iov_base = self->buffer;
+				iovec[1].iov_len = self->rp;
+				return 2;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->rp;
+				return 1;
+			}
+		}
+		if (self->len == self->rp) {
+			/*                       rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->rp;
+				return 1;
+			}
 
-		if ((0 > len) || ((0 == len) && !self->full)) {
-			iovec[0].iov_base = self->buffer + self->wp;
-			iovec[0].iov_len = self->len - self->wp;
-			iovec[1].iov_base = self->buffer;
-			iovec[1].iov_len = self->rp;
-			return 2;
-		} else if (0 < len) {
-			iovec[0].iov_base = self->buffer + self->wp;
-			iovec[0].iov_len = len;
-			return 1;
+			/*                       rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->wp < self->len)) {
+				iovec[0].iov_base = self->buffer + self->wp;
+				iovec[0].iov_len = self->rp - self->wp;
+				return 1;
+			}
+
+			/*                       rp
+			 * ||--------------------||
+			 *                       wp */
+			if ((self->wp == self->rp) && !self->full) {
+				iovec[0].iov_base = self->buffer;
+				iovec[0].iov_len = self->rp;
+				return 1;
+			}
 		}
 	}
 
@@ -122,13 +386,124 @@ void
 hev_ring_buffer_write_finish (HevRingBuffer *self, size_t inc_len)
 {
 	if (self && (0 < inc_len)) {
-		size_t p = inc_len + self->wp;
-		if (self->len < p)
-		  self->wp = p - self->len;
-		else
-		  self->wp = p;
-		if (self->wp == self->rp)
-		  self->full = true;
+		if (0 == self->rp) {
+			/* rp
+			 * ||--------------------||
+			 * wp */
+			if ((self->rp == self->wp) && !self->full) {
+				assert (self->len >= inc_len);
+				self->wp = inc_len;
+				if (inc_len == self->len)
+				  self->full = true;
+				return;
+			}
+			/* rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->len > self->wp)) {
+				assert ((self->len - self->wp) >= inc_len);
+				self->wp += inc_len;
+				if (self->wp == self->len)
+				  self->full = true;
+				return;
+			}
+			/* rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				return;
+			}
+		}
+		if ((0 < self->rp) && (self->len > self->rp)) {
+			/*           rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				assert (self->rp >= inc_len);
+				self->wp += inc_len;
+				if (self->wp == self->rp)
+				  self->full = true;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *      wp */
+			if ((0 < self->wp) && (self->wp < self->rp)) {
+				assert ((self->rp - self->wp) >= inc_len);
+				self->wp += inc_len;
+				if (self->wp == self->rp)
+				  self->full = true;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *           wp */
+			if ((self->wp == self->rp) && !self->full) {
+				assert (self->len >= inc_len);
+				self->wp += inc_len;
+				if (self->wp > self->len)
+				  self->wp -= self->len;
+				if (self->len == inc_len)
+				  self->full = true;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                wp */
+			if ((self->wp > self->rp) && (self->wp < self->len)) {
+				assert ((self->len - self->wp + self->rp) >= inc_len);
+				self->wp += inc_len;
+				if (self->wp > self->len)
+				  self->wp -= self->len;
+				if (self->wp == self->rp)
+				  self->full = true;
+				return;
+			}
+			/*           rp
+			 * ||--------------------||
+			 *                       wp */
+			if (self->len == self->wp) {
+				assert (self->rp >= inc_len);
+				self->wp = inc_len;
+				if (self->wp == self->rp)
+				  self->full = true;
+				return;
+			}
+		}
+		if (self->len == self->rp) {
+			/*                       rp
+			 * ||--------------------||
+			 * wp */
+			if (0 == self->wp) {
+				assert (self->rp >= inc_len);
+				self->wp = inc_len;
+				if (self->wp == self->rp)
+				  self->full = true;
+				return;
+			}
+
+			/*                       rp
+			 * ||--------------------||
+			 *           wp */
+			if ((0 < self->wp) && (self->wp < self->len)) {
+				assert ((self->len - self->wp) >= inc_len);
+				self->wp += inc_len;
+				if (self->wp == self->rp)
+				  self->full = true;
+				return;
+			}
+
+			/*                       rp
+			 * ||--------------------||
+			 *                       wp */
+			if ((self->wp == self->rp) && !self->full) {
+				assert (self->len >= inc_len);
+				self->wp = inc_len;
+				if (self->len == inc_len)
+				  self->full = true;
+				return;
+			}
+		}
 	}
 }
 
