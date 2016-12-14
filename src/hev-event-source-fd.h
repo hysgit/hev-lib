3a9d4ca2c13660d9bc333a2eb6b0633e67facd4f
Heiher <r@hev.cc>
2016-12-14 09:14:25 +0800

    HevEventSourceFD: Clean self pointer checking.

--- src/hev-event-source-fd.h
+++ src/hev-event-source-fd.h
@@ -48,43 +48,36 @@ _hev_event_source_fd_new (HevEventSource *source, int fd, uint32_t events)
 static inline HevEventSourceFD *
 _hev_event_source_fd_ref (HevEventSourceFD *self)
 {
-	if (self) {
-		self->_ref_count ++;
-		return self;
-	}
-
-	return NULL;
+	self->_ref_count ++;
+	return self;
 }
 
 static inline void
 _hev_event_source_fd_unref (HevEventSourceFD *self)
 {
-	if (self) {
-		self->_ref_count --;
-		if (0 == self->_ref_count) {
-			HEV_MEMORY_ALLOCATOR_FREE (self);
-		}
-	}
+	self->_ref_count --;
+	if (self->_ref_count)
+	  return;
+
+	HEV_MEMORY_ALLOCATOR_FREE (self);
 }
 
 static inline void
 hev_event_source_fd_set_data (HevEventSourceFD *self, void *data)
 {
-	if (self)
-	  self->data = data;
+	self->data = data;
 }
 
 static inline void *
 hev_event_source_fd_get_data (HevEventSourceFD *self)
 {
-	return self ? self->data : NULL;
+	return self->data;
 }
 
 static inline void
 _hev_event_source_fd_clear_source (HevEventSourceFD *self)
 {
-	if (self)
-	  self->source = NULL;
+	self->source = NULL;
 }
 
 #endif /* __HEV_EVENT_SOURCE_H__ */
